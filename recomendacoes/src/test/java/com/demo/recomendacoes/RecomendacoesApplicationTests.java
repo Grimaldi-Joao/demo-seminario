38e5471c255431e7318b4161b2f622aae5f34656
Grimaldi-Joao <joaogrimaldi6@gmail.com>
2025-11-09 15:37:17 -0300

    Criação do dockerfiler

---
+++ recomendacoes/src/test/java/com/demo/recomendacoes/RecomendacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.recomendacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
